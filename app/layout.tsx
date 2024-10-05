e1a53e2d41f3564c3698055db9ef9a674e9b015b
smaalfadly3 <smaalfadly@gmail.com>
2024-10-05 10:30:34 +0400

    layout childern by main tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-      <Navbar/>
-      {children}
-      <Footer/>
+        <Navbar/>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer/>
       </body>
     </html>
-  );
+  )
 }
