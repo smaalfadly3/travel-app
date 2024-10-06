48a7049e23b208d8999397da13ff514104e21def
smaalfadly3 <smaalfadly@gmail.com>
2024-10-06 20:58:16 +0400

    home page: create components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
 import Image from "next/image";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
-    <h1 className=" font-bold text-teal-500 text-2xl">
-      Hello World
-    </h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   )
 }
