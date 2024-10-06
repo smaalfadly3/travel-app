9dfe974e3b4a0ebd652939be0c2a7c83ed148ac8
smaalfadly3 <smaalfadly@gmail.com>
2024-10-06 18:38:25 +0400

    navbar: navigation list

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link"
 import Image from "next/image"
+import { NAV_LINKS } from "@/constants"
 
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/"> 
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/> 
-        <ul className=" hidden h-full gap-12 lg:flex">
-
-        </ul>
       </Link>
+      <ul className=" hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link)=>(
+            <Link href={link.href} key={link.key} className=" regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+              {link.label}
+            </Link>
+          ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+      </div>
     </nav>
   )
 }
