511d5c0ea771f5d0ded07de718ea0c0536828366
smaalfadly3 <smaalfadly@gmail.com>
2024-10-09 18:01:01 +0400

    home page: features  section  edits

--- components/Features.tsx
+++ components/Features.tsx
@@ -23,19 +23,19 @@ const Features = () => {
                     <h2 className=' bold-40 lg:bold-64'>Our Features</h2>
 
                 </div>
-            </div>
-            <ul className=' mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20'>
-                {FEATURES.map((feature)=>(
-                    <FeatureItem 
-                        key={feature.title}
-                        title={feature.title}
-                        icon={feature.icon}
-                        variant={feature.variant}
-                        description={feature.description}
-                    />
-                ))}
-            </ul>
+                <ul className=' mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20'>
+                    {FEATURES.map((feature)=>(
+                        <FeatureItem 
+                            key={feature.title}
+                            title={feature.title}
+                            icon={feature.icon}
+                            variant={feature.variant}
+                            description={feature.description}
+                        />
+                    ))}
+                </ul>
         </div>
+    </div>
     </section>
   )
 }
